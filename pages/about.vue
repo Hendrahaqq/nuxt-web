e35b7c13752a0ee7e628ffda229fd63384a0c5da
Muhammad Hendra Nailul Haqq <hendrahaqq@gmail.com>
2021-10-06 17:01:34 +0700

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="flex w-full h-screen justify-center items-center">
+    <router-link to="/">Back</router-link>
+  <p class="text-red-500 font-bold">About Page</p>
+</div>
+  
+  
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+
+<div class="w-full h-screen justify-center items-center">
+<div class="grid grid-cols-8">
+  <NuxtLogo/>
+</div>
+  <p class="text-red-500 font-bold"> {{title}} </p>
+
+  <router-link to="/about">About</router-link>
+</div>
+  
+  
+</template>
+
+<script>
+// import NuxtLogo from '/components/NuxtLogo.vue'
+export default {
+  data() {
+      return {
+        title: 'Home page'
+      }
+    },
+   head: {
+    title: 'Home page',
+    meta: [
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Home page description'
+      }
+    ],
+  }
+  
+}
+
+</script>
